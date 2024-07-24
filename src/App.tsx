5552f8ea0faf78d1d52fa56771c9af2e36118092
ndkienhd2004 <kienhd2004@gmail.com>
2024-07-24 13:48:46 +0700

    generate bottom-tab for winglets app

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,39 @@
-import {StyleSheet, View} from 'react-native';
-import MatchesScreen from './Screens/MatchesScreen';
-import HomeScreen from './Screens/HomeScreen';
+import React from 'react';
+import {StyleSheet} from 'react-native';
+import {NavigationContainer} from '@react-navigation/native';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
+import HomeScreen from './Screens/HomeScreen';
+import MatchesScreen from './Screens/MatchesScreen';
+import CustomTabBar from './Components/CustomTabBar';
+
+const Tab = createBottomTabNavigator();
+
 const App = () => {
   return (
-    <GestureHandlerRootView style={styles.pageContainer}>
-      <HomeScreen />
-      {/* <MatchesScreen /> */}
+    <GestureHandlerRootView style={styles.container}>
+      <NavigationContainer>
+        <Tab.Navigator
+          tabBar={props => <CustomTabBar {...props} />}
+          screenOptions={{
+            headerShown: false,
+            tabBarLabelStyle: {
+              fontSize: 16,
+              fontWeight: 500,
+              paddingTop: 0,
+            },
+            tabBarActiveTintColor: '#E73688',
+          }}>
+          <Tab.Screen name="Home" component={HomeScreen} />
+          <Tab.Screen name="Matches" component={MatchesScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </GestureHandlerRootView>
   );
 };
 
 const styles = StyleSheet.create({
-  pageContainer: {
-    alignItems: 'center',
-    justifyContent: 'center',
+  container: {
     flex: 1,
   },
 });
